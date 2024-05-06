49e341ff4d90e63df8e3972a29826c90ec8cf497
Allan James Chesarone <>
2024-05-06 15:46:40 -0400

    Fix current tests

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,9 +1,15 @@
 import { render, screen } from '@testing-library/react';
 
+import defaultTheme from 'app/theme';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
 
 it('renders page title', () => {
-  render(<App />);
+  render(
+    <ThemeProvider theme={defaultTheme}>
+      <App />
+    </ThemeProvider>,
+  );
   const titleElement = screen.getByText(/Close Frontend Take-Home Project/i);
   expect(titleElement).toBeInTheDocument();
 });
--- src/components/screens/Board/index.test.tsx
+++ src/components/screens/Board/index.test.tsx
@@ -1,9 +1,15 @@
 import { render, screen } from '@testing-library/react';
 
+import defaultTheme from 'app/theme';
+import { ThemeProvider } from 'styled-components';
 import Board from '.';
 
 it('renders columns', () => {
-  render(<Board />);
+  render(
+    <ThemeProvider theme={defaultTheme}>
+      <Board />
+    </ThemeProvider>,
+  );
   expect(screen.getByText(/To do/i)).toBeInTheDocument();
   expect(screen.getByText(/In progress/i)).toBeInTheDocument();
   expect(screen.getByText(/Done/i)).toBeInTheDocument();
